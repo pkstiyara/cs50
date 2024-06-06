17c6b77057b518f35f0a5c1689d15a93b853a42b
admin <admin@gmail.com>
2024-06-06 10:37:07 +0900

    initial

---
+++ mew.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// int counter = 3;
+// while (counter > 0)
+// {
+//     printf("meow\n");
+//     i++;
+// }
+
+
+
+int main(void)
+{
+    int i = 0;
+    while ( i < 3)
+    {
+        printf("meow\n");
+        i++;
+    }
+}
